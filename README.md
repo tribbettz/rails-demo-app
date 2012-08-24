dbef00f9c0ba30e3f1000e7233ce7bba0315f880
Zachary Tribbett <tribbettz.dev@gmail.com>
2012-08-24 16:51:12 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ruby on Rails Tutorial, Hartl Book: Demo Application
+
+This is the second application for
+[*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org/)
+by [Michael Hartl](http://michaelhartl.com/).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '89292a0ca741b7147bdd697b799aadb021ab88bc5da50043d2eae719efc0427551a716e5c95bcaaaca1e0583b7ebd15ab680d29b6c753dded1221bfea6cd69af'
